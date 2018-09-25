cf253a14770aaad99a26b88a86cb16d3a97bc6cd
Administrator <Administrator@pc.localdomain>
2018-09-25 19:23:57 +0200

    Ya tenemos la estrucutura SOMOS unos VIKINGOS

--- index.php
+++ index.php
@@ -8,14 +8,54 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <body style="background-color: #666">
-        <div class="container">  
-             <div class="row"> 
+        <body >
+        <div class="container">
+            <br>
+             <div class="row" style="background-color: #666"> 
                  <div class="col-12">  
-                 <h2 class="text-center" style="color:white;"> EJEMPLO DE INICIO DE SESIÓN EN PHP</h2>
+                 <h2 class="text-center" style="color:white;"><img src="imagenes/LOGO (1).png" class="img-fluid" alt="Responsive image"> Trival Preparatorio EVAU</h2>
+                 
                 </div> 
+             </div>
+            
+            
+           
+             <div class="row" style="background-color: #666"> 
+                 
+                     <div class="col-9"><h5 style="color:white;"> Elige una Asignatura para ponerte aprueba a ti mismo  </h5></div>
+                     <div class="col-3"><img src="imagenes/incioSesion.png" class="img-fluid" alt="Responsive image"></div>
+                     <br>
+                     <div class="col-2"></div>
+                     <div class="col-8">
+                     <button id="boton1" class="btn btn-primary btn-block">Primary</button>
+                    <br>
+                    <br>
+                    <button id="boton1" class="btn btn-primary btn-block">Primary</button>
+                    <br>
+                    <br>
+                    <button id="boton1" class="btn btn-primary btn-block">Primary</button>
+                    <br>
+                     </div>
+                     <div class="col-2"></div>
+                     
+                    
+                
+                 
+                
+             </div>
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        </div>
         <?php
         // put your code here
         ?>
